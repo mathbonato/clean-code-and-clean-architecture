ae2f8f5268698baf08cfe91f3ca54846e084e999
Matheus Bonato <matheusmbonato@gmail.com>
2022-03-08 22:09:59 -0300

    adding automated tests for freight calculation.

--- test/Order.test.ts
+++ test/Order.test.ts
@@ -41,9 +41,16 @@ test("Deve criar um pedido com três itens com cupom de desconto expirado", func
 
 test("Deve criar um pedido com três itens e calcular o frete", function () {
     const order = new Order("935.411.347-80");
-    order.addItem(new Item(1, "Instrumentos Musicais", "Guitarra", 1000, new Dimension(100, 30, 10)), 1);
-    order.addItem(new Item(2, "Instrumentos Musicais", "Amplificador", 5000, new Dimension(100, 50, 50)), 20);
-    order.addItem(new Item(3, "Instrumentos Musicais", "Cabo", 30, new Dimension(10, 10, 10), 3), 1);
+    order.addItem(new Item(1, "Instrumentos Musicais", "Guitarra", 1000, new Dimension(100, 30, 10), 3), 1);
+    order.addItem(new Item(2, "Instrumentos Musicais", "Amplificador", 5000, new Dimension(100, 50, 50), 20), 1);
+    order.addItem(new Item(3, "Instrumentos Musicais", "Cabo", 30, new Dimension(10, 10, 10), 1), 3);
     const total = order.getTotal();
     expect(total).toBe(6350);
+});
+
+test("Deve criar um pedido com três itens e calcular o frete minímo", function () {
+    const order = new Order("935.411.347-80");
+    order.addItem(new Item(1, "Instrumentos Musicais", "Cabo", 30, new Dimension(10, 10, 10), 0.9), 1);
+    const total = order.getTotal();
+    expect(total).toBe(40);
 });
