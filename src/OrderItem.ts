5c4c1870390ac4020901555d2ce5d4c606bfcb9f
Matheus Bonato <matheusmbonato@gmail.com>
2022-03-10 00:31:29 -0300

    adding use case for creating an order

--- src/OrderItem.ts
+++ src/OrderItem.ts
@@ -1,7 +1,5 @@
 export default class OrderItem {
-    constructor (readonly idItem: number, readonly price: number, readonly quantity: number) {
-
-    }
+    constructor (readonly idItem: number, readonly price: number, readonly quantity: number) {}
     
     getTotal() {
         return this.price * this.quantity;
