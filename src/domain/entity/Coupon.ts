abc642154fd846fb04ca137f79a958a33dda70cc
Matheus Bonato <matheusmbonato@gmail.com>
2022-03-13 23:29:50 -0300

    Adding use case coupon validation

--- src/domain/entity/Coupon.ts
+++ src/domain/entity/Coupon.ts
@@ -1,14 +1,14 @@
 export default class Coupon {
-    constructor (readonly code: string, readonly percentage: number, readonly expireDate?: Date) {
 
-    }
+	constructor (readonly code: string, readonly percentage: number, readonly expireDate?: Date) {
+	}
 
-    isExpired (today: Date = new Date()) {
-        if (!this.expireDate) return false;
-        return this.expireDate.getTime() < today.getTime();
-    }
+	isExpired (today: Date = new Date()) {
+		if (!this.expireDate) return false;
+		return this.expireDate.getTime() < today.getTime();
+	}
 
-    calculateDisccount (amount: number) {
-        return (amount * this.percentage)/100;
-    }
+	calculateDiscount (amount: number) {
+		return (amount *  this.percentage) / 100;
+	}
 }
